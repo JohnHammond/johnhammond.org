863729bfad600f90f6359c7e050ae5035ad119b9
John Hammond <johnhammond010@gmail.com>
2018-08-14 10:27:21 -0400

    Added pictures for Discord and scripts to install Flask

---
+++ dev/install_flask_and_gunicorn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$UID" -ne "0" ]
+then
+    echo "You must be root to run this script!"
+    exit
+fi
+
+
+apt-get update
+apt-get install -y python-pip python-dev
+
+pip install gunicorn flask
