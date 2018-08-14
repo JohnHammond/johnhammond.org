2e03277a69c7fd5136111fb0134b98e2cf86852a
John Hammond <johnhammond010@gmail.com>
2018-08-14 12:11:49 -0400

    Setup Google Ads thing again...

--- flask_server.py
+++ flask_server.py
@@ -14,6 +14,9 @@ def page_not_found(e): return render_template('custom_errors/custom_404.html'), 
 def index():
     return render_template('index.html')
 
+@app.route('/google571b7db071ec7f44.html')
+def adsbygoogle(): return render_template('google571b7db071ec7f44.html')
+
 @app.route('/programming')
 def programming(): return render_template('programming.html')
 
@@ -22,6 +25,9 @@ def writeups(): return render_template('writeups.html')
 
 @app.route('/voice_acting')
 def voice_acting(): return render_template('voice_acting.html')
+
+@app.route('/graphic_design')
+def graphic_design(): return render_template('graphic_design.html')
 
 
 if ( __name__ == "__main__" ):
